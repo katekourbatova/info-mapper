ada8cf71d60086f5203ae81c5bd57d52b184c47b
Kate Kourbatova <email": null>
2017-01-20 14:56:36 -0800

    Implement home page showing map focused on Sydney, Australia, because why not.

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,17 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+// $(document).ready(function() {
+//   // This is called after the document has loaded in its entirety
+//   // This guarantees that any elements we bind to will exist on the page
+//   // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-});
+//   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+// });
+
+///////////////Google maps example code//////////////
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
